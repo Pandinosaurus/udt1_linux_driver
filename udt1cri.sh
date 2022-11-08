a88081dc7d5d88547a6d988d4f571a9c45fb59cf
sebcaux <sebastien.caux@uniswarm.eu>
2022-11-08 14:13:46 +0100

    Added support for multiple interface

--- udt1cri.sh
+++ udt1cri.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 modprobe udt1cri_usb
-sudo ip link set can0 type can bitrate 1000000
-sudo ip link set can0 up
+
+if [[ ! -f "/dev/can0" ]]; then
+    sudo ip link set can0 type can bitrate 1000000
+    sudo ip link set can0 up
+fi
+if [[ ! -f "/dev/can1" ]]; then
+    sudo ip link set can1 type can bitrate 1000000
+    sudo ip link set can1 up
+fi
+if [[ ! -f "/dev/can2" ]]; then
+    sudo ip link set can2 type can bitrate 1000000
+    sudo ip link set can2 up
+fi
+if [[ ! -f "/dev/can3" ]]; then
+    sudo ip link set can3 type can bitrate 1000000
+    sudo ip link set can3 up
+fi
